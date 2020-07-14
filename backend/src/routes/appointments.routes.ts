0dee60d824dfd8324f3f96e96cf2da8bd3a2117d
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-07-14 06:18:49 -0400

    Conceito de Repositorio

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
-
+const appointmentsRepository = new AppointmentsRepository();
 appointmentsRouter.use('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parseDate = startOfHour(parseISO(date));
 
-    const findAppointmentInSameDate = appointments.find(appointment =>
-        isEqual(parseDate, appointment.date),
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(
+        parseDate,
     );
 
     if (findAppointmentInSameDate) {
@@ -22,9 +20,7 @@ appointmentsRouter.use('/', (request, response) => {
             .json({ message: 'This appointment is already booked' });
     }
 
-    const appointment = new Appointment(provider, parseDate);
-
-    appointments.push(appointment);
+    const appointment = appointmentsRepository.create(provider, parseDate);
 
     return response.json(appointment);
 });
