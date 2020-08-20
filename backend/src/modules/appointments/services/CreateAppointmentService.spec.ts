06567b2d6101db919504c5675b025c554becf10c
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-08-19 21:54:18 -0400

    Scheduling test

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,8 +20,25 @@ describe('CreateAppointment', () => {
     });
 });
 
-// describe('CreateAppointment', () => {
-//    it('should not be able to create tow appointment on the same time', () => {
-//        expect(1 + 2).toBe(3);
-//    });
-// });
+describe('CreateAppointment', () => {
+    it('should not be able to create tow appointment on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123456',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
