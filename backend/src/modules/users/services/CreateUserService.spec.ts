2aa2d82806149a38c0638f35ed112462e1fe51df
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-08-26 20:50:47 -0400

    Tests refactoring

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -4,15 +4,21 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+
 describe('CreateUser', () => {
-    it('should be able to create a new appointment', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeHashProvider = new FakeHashProvider();
-        const createUser = new CreateUserService(
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeHashProvider = new FakeHashProvider();
+        createUser = new CreateUserService(
             fakeUsersRepository,
             fakeHashProvider,
         );
+    });
 
+    it('should be able to create a new appointment', async () => {
         const user = await createUser.execute({
             name: 'john Doe',
             email: 'johndoe@exemple.com',
@@ -23,20 +29,13 @@ describe('CreateUser', () => {
     });
 
     it('should not be able to create a new user with same email from another', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeHashProvider = new FakeHashProvider();
-        const createUser = new CreateUserService(
-            fakeUsersRepository,
-            fakeHashProvider,
-        );
-
         await createUser.execute({
             name: 'john Doe',
             email: 'johndoe@exemple.com',
             password: '123456',
         });
 
-        expect(
+        await expect(
             createUser.execute({
                 name: 'john Doe',
                 email: 'johndoe@exemple.com',
