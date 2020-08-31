8d20f7e8640c6a366ef8ab3f08993b2e24a66887
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-08-30 22:00:56 -0400

    Filtering appointments by month

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,9 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-    user_id: string;
+    provider_id: string;
     year: number;
     month: number;
 }
@@ -15,9 +15,26 @@ type IResponse = Array<{
 
 @injectable()
 class ListProviderMonthAvailabilityService {
-    constructor() {}
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {}
 
-    public async execute({ user_id }: IRequest): Promise<IResponse> {
+    public async execute({
+        provider_id,
+        year,
+        month,
+    }: IRequest): Promise<IResponse> {
+        const appointments = await this.appointmentsRepository.findAllInMonthFromProviderDTO(
+            {
+                provider_id,
+                year,
+                month,
+            },
+        );
+
+        console.log(appointments);
+
         return [{ day: 1, availability: false }];
     }
 }
