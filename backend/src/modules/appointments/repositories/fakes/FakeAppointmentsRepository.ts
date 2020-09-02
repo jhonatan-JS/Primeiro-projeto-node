8a321684b313f1fd778bbb4f16ab6172ffeebf14
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-01 20:22:11 -0400

    Listing available times

--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { uuid } from 'uuidv4';
-import { isEqual, getMonth, getYear } from 'date-fns';
+import { isEqual, getMonth, getYear, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -18,7 +19,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         return findAppointment;
     }
 
-    public async findAllInMonthFromProviderDTO({
+    public async findAllInMonthFromProvider({
         provider_id,
         month,
         year,
@@ -34,21 +35,39 @@ class AppointmentsRepository implements IAppointmentsRepository {
         return appointments;
     }
 
+    public async findAllInDayFromProvider({
+        provider_id,
+        day,
+        month,
+        year,
+    }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+        const appointments = this.appointments.filter(appointment => {
+            return (
+                appointment.provider_id === provider_id &&
+                getDate(appointment.date) === day &&
+                getMonth(appointment.date) + 1 === month &&
+                getYear(appointment.date) === year
+            );
+        });
+
+        return appointments;
+    }
+
     public async create({
         provider_id,
         date,
     }: ICreateAppointmentDTO): Promise<Appointment> {
-        const appointment = new Appointment();
+        const appointments = new Appointment();
 
-        Object.assign(appointment, { id: uuid(), date, provider_id });
+        Object.assign(appointments, { id: uuid(), date, provider_id });
 
         // appointment.id = uuid();
         // appointment.date = date;
         // appointment.provider_id = provider_id;
 
-        this.appointments.push(appointment);
+        this.appointments.push(appointments);
 
-        return appointment;
+        return appointments;
     }
 }
 
