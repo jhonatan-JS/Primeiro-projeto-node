cbd70b306d66f57966ac99c249564172d340e03d
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-04 19:02:03 -0400

    Schedule rules correction

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,11 +16,11 @@ describe('CreateAppointment', () => {
 
     it('should be able to create a new appointment', async () => {
         jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-            return new Date(2020, 4, 10, 12).getTime();
+            return new Date(2020, 9, 10, 12).getTime();
         });
 
         const appointment = await createAppointment.execute({
-            date: new Date(2020, 4, 10, 13),
+            date: new Date(2020, 9, 10, 13),
             user_id: '123456',
             provider_id: '123456',
         });
@@ -31,7 +31,7 @@ describe('CreateAppointment', () => {
     });
 
     it('should not be able to create tow appointment on the same time', async () => {
-        const appointmentDate = new Date(2020, 4, 10, 11);
+        const appointmentDate = new Date(2020, 9, 10, 11);
 
         await createAppointment.execute({
             date: appointmentDate,
@@ -50,12 +50,12 @@ describe('CreateAppointment', () => {
 
     it('should not be able to create an appointment on a past date', async () => {
         jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-            return new Date(2020, 4, 10, 12).getTime();
+            return new Date(2020, 8, 10, 12).getTime();
         });
 
         await expect(
             createAppointment.execute({
-                date: new Date(2020, 4, 10, 11),
+                date: new Date(2020, 8, 10, 11),
                 user_id: '123456',
                 provider_id: '123456',
             }),
