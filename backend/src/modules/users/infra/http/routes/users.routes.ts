f3a0297a39e6d478bbeb974ac88fe38edca5a18b
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-06 12:18:48 -0400

    Validating data

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UserController from '../controllers/UsersControlle';
 import UserAvatarController from '../controllers/UserAvatarController';
@@ -13,7 +14,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-userRouter.post('/', userController.create);
+userRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+        },
+    }),
+    userController.create,
+);
 
 userRouter.patch(
     '/avatar',
