3be95bbcaaf922647cd18162c74a06759cc2c3f1
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-08 06:37:43 -0400

    Invalidating cache

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import ICashProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUserRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
@@ -17,8 +18,12 @@ class CreateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUserRepository,
+
         @inject('HashProvider')
         private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICashProvider,
     ) {}
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -35,6 +40,8 @@ class CreateUserService {
             email,
             password: hashdPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix('providers-list');
 
         return user;
     }
--- backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -6,7 +6,7 @@ export default class RedisCacheProvider implements ICacheProvider {
     private client: RedisClient;
 
     constructor() {
-        this.client = Redis(cacheConfig.config.redis);
+        this.client = new Redis(cacheConfig.config.redis);
     }
 
     public async save(key: string, value: any): Promise<void> {
@@ -19,10 +19,23 @@ export default class RedisCacheProvider implements ICacheProvider {
         if (!data) {
             return null;
         }
+
         const parsedData = JSON.parse(data) as T;
 
         return parsedData;
     }
 
     public async invalidate(key: string): Promise<void> {}
+
+    public async invalidatePrefix(prefix: string): Promise<void> {
+        const keys = await this.client.keys(`${prefix}:*`);
+
+        const pipeline = this.client.pipeline();
+
+        keys.forEach(key => {
+            pipeline.del(key);
+        });
+
+        await pipeline.exec();
+    }
 }
