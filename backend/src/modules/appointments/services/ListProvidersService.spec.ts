0ddfbaf923cf3cc0be26c527a73821d92c1765c4
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-09 05:11:19 -0400

    Test refactoring

--- backend/src/modules/appointments/services/ListProvidersService.spec.ts
+++ backend/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,14 +1,20 @@
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import ListProvidersService from './ListProviderService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let listProviders: ListProvidersService;
+let fakeCacheProvider: FakeCacheProvider;
 
 describe('UpdateProfile', () => {
     beforeEach(() => {
+        fakeCacheProvider = new FakeCacheProvider();
         fakeUsersRepository = new FakeUsersRepository();
 
-        listProviders = new ListProvidersService(fakeUsersRepository);
+        listProviders = new ListProvidersService(
+            fakeUsersRepository,
+            fakeCacheProvider,
+        );
     });
 
     it('should be able to this the providers', async () => {
