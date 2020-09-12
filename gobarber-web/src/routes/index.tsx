3a7536bd4dcee7c7e994795cfc7b86ee73fdb861
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-09-12 15:26:24 -0400

    Creating recovery page

--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboad from '../pages/Dashbord';
 
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
 
     <Route path="/dashboard" component={Dashboad} isPrivate />
   </Switch>
